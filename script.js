07f647b2adc2115aa2a2951baa59cf5917a6b299
hamzailyas237 <hamzailyas237@gmail.com>
2023-10-27 09:41:05 +0500

    fixed: All checkboxes being ticked on refresh

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 // script.js
-const checkboxes = document.querySelectorAll('.checkbox');
-
+const checkboxes = document.querySelectorAll(".checkbox");
 // Add event listeners to checkboxes
 checkboxes.forEach((checkbox) => {
-  checkbox.addEventListener('change', (event) => {
-    const checklistName = event.target.getAttribute('data-checklist');
-    const itemIndex = [...checkbox.parentElement.parentElement.children].indexOf(checkbox.parentElement);
-
-    // Save the completion status to local storage
-    localStorage.setItem(`${checklistName}_${itemIndex}`, event.target.checked);
-  });
-
   // Retrieve the completion status from local storage
-  const checklistName = checkbox.getAttribute('data-checklist');
-  const itemIndex = [...checkbox.parentElement.parentElement.children].indexOf(checkbox.parentElement);
-  const isChecked = localStorage.getItem(`${checklistName}_${itemIndex}`);
-  if (isChecked === 'true') {
-    checkbox.checked = true;
+  const checklistName = checkbox.getAttribute("data-checklist");
+  const itemIndex = [...checkbox.parentElement.parentElement.children].indexOf(
+    checkbox.parentElement
+  );
+
+  if (checkbox.checked) {
+    localStorage.removeItem(`${checklistName}_${itemIndex}`);
+    checkbox.checked = false;
+  } else {
+    const isChecked = localStorage.getItem(`${checklistName}_${itemIndex}`);
+    if (isChecked === "true") {
+      checkbox.checked = true;
+    }
   }
 });
 
@@ -37,11 +36,9 @@ clickedGlowElements.forEach((element) => {
   });
 });
 
-
 // Code no longer user since clicking the section title does this action
 // and section title encompasses the arrow
 // drop down arrow
-
 
 // const dropdownArrows = document.querySelectorAll(".dropdown-arrow");
 
@@ -53,9 +50,6 @@ clickedGlowElements.forEach((element) => {
 //     arrow.classList.toggle("checked");
 //   });
 // });
-
-
-
 
 // Code no longer user since clicking the section title does this action
 // and section title encompasses the arrow
@@ -81,32 +75,35 @@ clickedGlowElements.forEach((element) => {
 // });
 
 // Add click event listener to checklist section titles
-const sectionTitles = document.querySelectorAll('.section-title-label');
+const sectionTitles = document.querySelectorAll(".section-title-label");
 sectionTitles.forEach((label) => {
-  label.addEventListener('click', (e) => {
-    // prevent the label from triggering default behavior of toggling the checkbox
-    e.preventDefault();
+  label.addEventListener(
+    "click",
+    (e) => {
+      // prevent the label from triggering default behavior of toggling the checkbox
+      e.preventDefault();
 
-    // Expand section and change the arrow icon
-    const arrow = label.querySelector('[class*="dropdown-arrow"]');
-    const listItem = arrow.closest("li");
-    arrow.classList.toggle("checked");
+      // Expand section and change the arrow icon
+      const arrow = label.querySelector('[class*="dropdown-arrow"]');
+      const listItem = arrow.closest("li");
+      arrow.classList.toggle("checked");
 
-    const hiddenText = listItem.querySelector(".hidden-text");
-    hiddenText.classList.toggle("show");  // show hidden text
+      const hiddenText = listItem.querySelector(".hidden-text");
+      hiddenText.classList.toggle("show"); // show hidden text
 
-    const checklistItem = arrow.closest(".checklist-item");
-    if (arrow.className === "dropdown-arrow checked") {
-      hiddenText.style.display = "block";
-      const hiddenTextHeight = hiddenText.clientHeight;
-      checklistItem.style.marginBottom = hiddenTextHeight + "px";
-    } else {
-      hiddenText.style.display = "none";
-      checklistItem.style.marginBottom = "0";
-    }
-  }, false);
+      const checklistItem = arrow.closest(".checklist-item");
+      if (arrow.className === "dropdown-arrow checked") {
+        hiddenText.style.display = "block";
+        const hiddenTextHeight = hiddenText.clientHeight;
+        checklistItem.style.marginBottom = hiddenTextHeight + "px";
+      } else {
+        hiddenText.style.display = "none";
+        checklistItem.style.marginBottom = "0";
+      }
+    },
+    false
+  );
 });
-
 
 // New quote on Refresh
 
@@ -125,26 +122,30 @@ sectionTitles.forEach((label) => {
 // });
 
 async function logMovies() {
-  const response = await fetch("https://api.api-ninjas.com/v1/quotes?category=happiness",
- { headers: { 'X-Api-Key': 'gv414CStL2w22HtsC4pp8A==fHW82xPwAxJPaUjU'},});
+  const response = await fetch(
+    "https://api.api-ninjas.com/v1/quotes?category=happiness",
+    { headers: { "X-Api-Key": "gv414CStL2w22HtsC4pp8A==fHW82xPwAxJPaUjU" } }
+  );
   const movies = await response.json();
   document.getElementById("quoteContent").textContent = movies[0].quote;
-  document.getElementById("quoteTitle").textContent = "- "+ movies[0].author;
+  document.getElementById("quoteTitle").textContent = "- " + movies[0].author;
 }
 
 logMovies();
 
 // java basic
 function copyToClipboard(exampleNumber) {
-  const codeBlock = document.querySelector(`.code-snippet[data-example="${exampleNumber}"]`);
+  const codeBlock = document.querySelector(
+    `.code-snippet[data-example="${exampleNumber}"]`
+  );
   if (codeBlock) {
     const codeText = codeBlock.textContent;
-    const textArea = document.createElement('textarea');
+    const textArea = document.createElement("textarea");
     textArea.value = codeText;
     document.body.appendChild(textArea);
     textArea.select();
-    document.execCommand('copy');
+    document.execCommand("copy");
     document.body.removeChild(textArea);
-    alert('Code copied to clipboard');
+    alert("Code copied to clipboard");
   }
 }
